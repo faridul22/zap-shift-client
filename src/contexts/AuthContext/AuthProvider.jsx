88fff2d7b0ea86a8e08ddf27edc8187f8365e883
faridul22 <developerfarid2122@gmail.com>
2026-07-15 23:03:07 +0600

    social login issue fixed

--- src/contexts/AuthContext/AuthProvider.jsx
+++ src/contexts/AuthContext/AuthProvider.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../firebase/firebase.init';
-import { GoogleAuthProvider } from 'firebase/auth/web-extension';
 
 const googleProvider = new GoogleAuthProvider();
 
